17895568c30cb0a24b73beeea6d4825162b2b790
Drake <dariocostarelli88@gmail.com>
2025-09-10 12:09:28 +0100

    Allow Heroku host in ALLOWED_HOSTS

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-)a5bh-olc)&78sw64z5nh^(et(^5i=e(pw3&0c3jvaa*+%t$w^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "django-udemy-firstproject-d6e6b59191bb.herokuapp.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
